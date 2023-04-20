d3b1880b93418d50040f167e6c7a53aa7bdbdaad
Hina <hinakouser152@gmail.com>
2023-04-20 13:02:23 +0530

    brackets changes

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -20,7 +20,7 @@ const TheirMessage = ({ lastMessage, message }) => {
         )
         : (
           <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
-            {message.text}
+            message.text
           </div>
         )}
     </div>
